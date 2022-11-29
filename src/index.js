3bc5109ac6bb99b06d91f7ecbfc99092d987a379
Kawan Costa Soares Miranda <VCP10002405@eptv.com.br>
2022-11-29 10:09:39 -0300

    Cadastro usuario

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const pathProdutos = "src/produtos/produtos.json";
+const pathUsuarios = "src/usuarios/usuarios.json";
+
 const encond = "utf-8";
+
 // DATABASE
 const fs = require('fs');
 
 //METODOS GENERICOS
-const dados_produtos = (path, tipo) => {
+const dados = (path, tipo) => {
     try {
         const dados = fs.readFileSync(path, tipo);
         dadosObj = JSON.parse(dados);
@@ -27,7 +30,7 @@ app.use(express.json());
 //ROTAS
 app.get('/produtos', (req, res) => { // GET ALL
     try {
-        return res.status(200).json(dados_produtos(pathProdutos, encond));
+        return res.status(200).json(dados(pathProdutos, encond));
     } catch (error) {
         return res.status(500).json("Ocorreu um erro");
     }
@@ -36,7 +39,7 @@ app.get('/produtos', (req, res) => { // GET ALL
 app.get('/produtos/:produtos_id', (req, res) => { // GET BY ID
     try {
         const { produtos_id } = req.params;
-        let produtosJson = dados_produtos(pathProdutos, encond);
+        let produtosJson = dados(pathProdutos, encond);
         const produto = produtosJson.find((produto) => produto.id == produtos_id);
         if (!produto) {
             return res.status(404).json("Produto Não Encontrado");
@@ -50,7 +53,7 @@ app.get('/produtos/:produtos_id', (req, res) => { // GET BY ID
 
 app.post('/produtos', (req, res) => { //CREATE
     try {
-        let array_produtos = dados_produtos(pathProdutos, encond); //Pegando dados 
+        let array_produtos = dados(pathProdutos, encond); //Pegando dados 
         const { nome, descricao, preco, img } = req.body;
         const idProduto = () => {
             let maiorID = 0;
@@ -72,11 +75,36 @@ app.post('/produtos', (req, res) => { //CREATE
     }
 });
 
+app.post('/api/cadastro', (req, res) => { //CREATE
+    try {
+        let array_usuarios = dados(pathUsuarios, encond); //Pegando dados 
+        const { nome, data_nascimento, cpf, telefone, email, senha, genero } = req.body;
+
+        const idUsuario = () => {
+            let maiorID = 0;
+            array_usuarios.forEach(element => {
+                if (typeof element.id == 'number') {
+                    maiorID = maiorID > element.id ? maiorID : element.id;
+                }
+            });
+            maiorID++;
+            return maiorID;
+        }
+        let id = idUsuario();
+        const usuario = { id, nome, data_nascimento, cpf, telefone, email, senha, genero };
+        array_usuarios.push(usuario);
+        fs.writeFileSync(pathUsuarios, convertJsonToString(array_usuarios), encond);
+        return res.status(201).json(usuario);
+    } catch (error) {
+        return res.status(500).json("Ocorreu um erro " + error);
+    }
+});
+
 app.patch('/produtos/:produtos_id', (req, res) => { //UPDATE
     try {
         const { nome, descricao, preco, img } = req.body;
         const { produtos_id } = req.params;
-        let array_produtos = dados_produtos(pathProdutos, encond); //Pegando dados 
+        let array_produtos = dados(pathProdutos, encond); //Pegando dados 
         const produto = array_produtos.find((produto) => produto.id == produtos_id);
         const produtos_filtrados = array_produtos.filter((produto) => produto.id != produtos_id);
         if (!produto) {
@@ -99,7 +127,7 @@ app.patch('/produtos/:produtos_id', (req, res) => { //UPDATE
 app.delete('/produtos/:produtos_id', (req, res) => { //DELETE
     try {
         const { produtos_id } = req.params;
-        let array_produtos = dados_produtos(pathProdutos, encond); //Pegando dados 
+        let array_produtos = dados(pathProdutos, encond); //Pegando dados 
         const existe = array_produtos.find(produto => produto.id == produtos_id);
         if (existe) {
             const produto_filtrado = array_produtos.filter(produto => produto.id != produtos_id);
